e512423b055d5a34475e1855fbbff5238311e2dd
imran <pathanimrankhan619@gmail.com>
2015-10-21 07:55:57 +0530

    backup

--- course/models.py
+++ course/models.py
@@ -7,6 +7,5 @@ class Catalog(models.Model):
     instructor = models.CharField(max_length=40)
     credits = models.SmallIntegerField(default=0)
     coursetag = models.CharField(max_length=1,default="C")  #department elective or core course
-
     def __str__(self):
         return str(self.code) + ": " + str(self.name) + " [" + str(self.credits) + "]"
